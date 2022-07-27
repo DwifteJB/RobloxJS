9cb74ec59d29747ba5f692b0f0d4895a6d74d37d
DwifteJB <dwifte@outlook.com>
2022-07-27 01:32:24 +0100

    completely finished

--- README.md
+++ README.md
@@ -1,6 +1,146 @@
 # RobloxJS
-Roblox API written as a nodejs module
+Roblox API written as a nodejs module.
+(sorry if any of this is wrong, i had a panic attack half way through lol)
+# How to use
 
+## Place Class
+
+### Get Details of Place
+
+<a href=https://github.com/S0ftwareUpd8/roblox-api#get-place-details>API Documentation</a>
+
+    const robloxjs = require("robloxjs")
+    (async () => {
+        let Place = await new robloxjs.Place(2506738224) // Place ID
+        let placeDetails = await Place.getDetails()
+    })(); // EVERYTHING MUST BE DONE IN AN ASYNC ENVIRONMENT!!
+
+### Get Settings of Place
+<a href=https://github.com/S0ftwareUpd8/roblox-api#get-place-settings>API Documentation</a>
+
+    const robloxjs = require("robloxjs")
+    (async () => {
+        let Place = await new robloxjs.Place(2506738224) // Place ID
+        let PlaceSettings = await Place.getSettings()
+    })(); // EVERYTHING MUST BE DONE IN AN ASYNC ENVIRONMENT!!
+
+### Get Universe ID of Place
+<a href=https://github.com/S0ftwareUpd8/roblox-api#get-universe-containing-place>API Documentation</a>
+
+    const robloxjs = require("robloxjs")
+    (async () => {
+        let Place = await new robloxjs.Place(2506738224) // Place ID
+        let UniverseJSON = await Place.getUniverse()
+    })(); // EVERYTHING MUST BE DONE IN AN ASYNC ENVIRONMENT!!
+
+### Get Game Instances
+<a href=https://games.roblox.com/docs#!/GameInstances/get_v1_games_placeId_servers_serverType>API Documentation</a>
+
+    const robloxjs = require("robloxjs")
+    (async () => {
+        let Place = await new robloxjs.Place(2506738224) // Place ID
+        let gameInstances = await Place.getGameInstances()
+    })(); // EVERYTHING MUST BE DONE IN AN ASYNC ENVIRONMENT!!
+
+## Player Class
+
+### Get Headshot player URL
+<a href=https://github.com/matthewdean/roblox-web-apis#search-apis>API DOCUMENTATION</a>
+
+<a href=https://github.com/matthewdean/roblox-web-apis#valid-thumbnail-sizes>Valid Thumbnail Sizes</a>
+
+
+    const robloxjs = require("robloxjs")
+    (async () => {
+        let Player = await new robloxjs.Player(1) // Place ID
+        let HeadshotURL = await Player.getHeadShotPicture(400,400)
+    })(); // EVERYTHING MUST BE DONE IN AN ASYNC ENVIRONMENT!!
+
+
+### Get Outfit Picture URL
+<a href=https://github.com/matthewdean/roblox-web-apis#search-apis>API DOCUMENTATION</a>
+
+<a href=https://github.com/matthewdean/roblox-web-apis#valid-thumbnail-sizes>Valid Thumbnail Sizes</a>
+
+
+    const robloxjs = require("robloxjs")
+    (async () => {
+        let Player = await new robloxjs.Player(1) // Place ID
+        let OutFitURL = await Player.getOutfitPicture(400,400)
+    })(); // EVERYTHING MUST BE DONE IN AN ASYNC ENVIRONMENT!!
+
+### Get Avatar Thumbnails
+<a href=https://github.com/matthewdean/roblox-web-apis#search-apis>API DOCUMENTATION</a>
+
+    const robloxjs = require("robloxjs")
+    (async () => {
+        let Player = await new robloxjs.Player(1) // Place ID
+        let ListOfAvatarThumbnails = await Player.getAvatarThumbnails()
+    })(); // EVERYTHING MUST BE DONE IN AN ASYNC ENVIRONMENT!!
+
+### Get Online Status
+<a href=https://github.com/S0ftwareUpd8/roblox-api#get-online-status-of-an-user>API DOCUMENTATION</a>
+
+    const robloxjs = require("robloxjs")
+    (async () => {
+        let Player = await new robloxjs.Player(1) // Place ID
+        let onlineStatus = await Player.getOnlineStatus()
+    })(); // EVERYTHING MUST BE DONE IN AN ASYNC ENVIRONMENT!!
+
+### Get Player's Created Games
+<a href=https://github.com/matthewdean/roblox-web-apis#get-a-users-profile-games>API DOCUMENTATION</a>
+
+    const robloxjs = require("robloxjs")
+    (async () => {
+        let Player = await new robloxjs.Player(1) // Place ID
+        let createdGames = await Player.getCreatedGames()
+    })(); // EVERYTHING MUST BE DONE IN AN ASYNC ENVIRONMENT!!
+
+### Get Player's General Info
+I completely forgot where I found this.
+
+    const robloxjs = require("robloxjs")
+    (async () => {
+        let Player = await new robloxjs.Player(1) // Place ID
+        let createdGames = await Player.getInfo()
+    })(); // EVERYTHING MUST BE DONE IN AN ASYNC ENVIRONMENT!!
+
+## Universe Class
+
+### How do I get a universe ID?
+
+Well, simply use the Get Universe ID of Place section from the Player Class
+
+### Get all places connected to Universe
+<a href=https://github.com/S0ftwareUpd8/roblox-api#get-places-in-an-universe>API DOCUMENTATION</a>
+
+    const robloxjs = require("robloxjs")
+    (async () => {
+        let Universe = await new robloxjs.Universe(1337) // Universe ID
+        let UniversePlaces = await Universe.getPlaces()
+    })(); // EVERYTHING MUST BE DONE IN AN ASYNC ENVIRONMENT!!
+
+### Get Info
+<a href=https://github.com/S0ftwareUpd8/roblox-api#get-info-about-an-universe>API DOCUMENTATION</a>
+
+    const robloxjs = require("robloxjs")
+    (async () => {
+        let Universe = await new robloxjs.Universe(1337) // Universe ID
+        let UniverseInfo = await Universe.getInfo()
+    })(); // EVERYTHING MUST BE DONE IN AN ASYNC ENVIRONMENT!!
+
+### Get Price of Game
+<a href=https://games.roblox.com/docs#!/Games/get_v1_games_games_product_info>API DOCUMENTATION</a>
+
+    const robloxjs = require("robloxjs")
+    (async () => {
+        let Universe = await new robloxjs.Universe(1337) // Universe ID
+        let UniversePrices = await Universe.getPrice()
+    })(); // EVERYTHING MUST BE DONE IN AN ASYNC ENVIRONMENT!!
+
+### Note
+
+**There is a "user" class that is currently unused in the index.js file, this is due to it not working and it is being looked on. If anyone could help our team look for a way to use it, that would be appreciated.**
 # Special Thanks
 
-Special thanks to <a href=https://github.com/S0ftwareUpd8/roblox-api>roblox-api</a> for the documentation on the API
+Special thanks to <a href=https://github.com/S0ftwareUpd8/roblox-api>roblox-api</a> & <a href=https://github.com/matthewdean/roblox-web-apis>roblox-web-apis</a> for the documentation on the API
--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 const fetch = require("node-fetch")
-async function fetchJSON(url,body,cookies) {
-    body = (!body || body.length == 0 || body == undefined || body == null) ? null : body;
-    cookies = (!cookies) ? null : cookies;
-    const options = {body:body,cookies:cookies}
-    // fetch a json file, also includes error handling
-    return new Promise((resolve,reject) => {
-        fetch(url, options)
-            .catch(error => reject(error))
-            .then(res => res);
-    })
-}
 
 class Player {
     constructor(playerID) {
         this.playerID = playerID;
-        if (this.checkValid() == false) {
+        if (this.checkValid() === false) {
             console.error(`This isn't a valid player, using Roblox's ID`);
             this.playerID = 1;
         }
@@ -22,38 +11,31 @@ class Player {
     }
 
     checkValid() {
-        return fetchJSON(`https://api.roblox.com/users/${this.playerID}/onlinestatus/`)
+        return fetch(`https://api.roblox.com/users/${this.playerID}/onlinestatus/`)
             .then(res => {
-
-                if (res.json().errors.code) {
+                if (res.json().errors || res == false) {
                     return false
                 } else return true
             })
     }
-    getGroups() {
-        return fetchJSON(`https://www.roblox.com/users/profile/playergroups-json?userId=${this.playerID}`)
-            .then(res => res.json())
+    getHeadShotPicture(width,height) {
+        return fetch(`https://www.roblox.com/bust-thumbnail/image?userId=${this.playerID}&width=${width}&height=${height}&format=png`).then(res => res.url)
+    }
+    getCreatedGames() {
+        return fetch(`https://www.roblox.com/users/profile/playergames-json?userId=${this.playerID}`).then(res => res.json())
+    }
+    getOutfitPicture(width,height) {
+        return fetch(`https://www.roblox.com/outfit-thumbnail/image?userOutfitId=${this.playerID}&width=${width}&height=${height}&format=png`).then(res => res.url)
+    }
+    getAvatarThumbnails() {
+        return fetch(`https://www.roblox.com/avatar-thumbnails?params=[{userId:${this.playerID}]`).then(res => res.json())
     }
     getOnlineStatus() {
-        return fetchJSON(`https://api.roblox.com/users/${this.playerID}/onlinestatus/`)
+        return fetch(`https://api.roblox.com/users/${this.playerID}/onlinestatus/`)
             .then(res => res.json())
     }
-
-    getPresence() {
-        return fetchJSON(`https://www.roblox.com/presence/user?userId=${this.playerID}`)
-            .then(res => res.json())
-    }
-
-    getMultipleUsersPresense(playerList) {
-        if (typeof (playerList) !== Array) return `Not a list specified.`;
-        let parsedList = "";
-        for (let player in playerList) {
-            if (parsedList.length == 0) {
-                parsedList = `userIds=${playerList[player]}`
-            }
-            parsedList = `${parsedList}&userIds=${playerList[player]}`
-        }
-        return fetchJSON(`https://www.roblox.com/presence/users?${parsedList}`).then(res => res.json());
+    getInfo() {
+        return fetch(`https://users.roblox.com/v1/users/${this.playerID}`).then(res => res.json())
     }
 }
 /*
@@ -62,6 +44,8 @@ class Player {
             UNDER. ANY. CIRCUMSTANCE.
 
             THANK YOU.
+
+            (User class currently doesnt work.)
  */
 class User {
     constructor(roblosecurity) {
@@ -70,7 +54,7 @@ class User {
         if (this.checkStatus() == false) return console.error(`Invalid roblosecurity token`);
     }
     checkStatus() {
-        return fetchJSON("https://www.roblox.com/my/account/json",null,`.ROBLOSECURITY=${this.roblosecurity}`)
+        return fetch("https://www.roblox.com/my/account/json",`.ROBLOSECURITY=${this.roblosecurity};`)
             .then(res => {
                 try {
                     let req = res.json()
@@ -81,22 +65,22 @@ class User {
             })
     }
     getFavouritedGames() {
-        return fetchJSON("https://www.roblox.com/user/favorites/places",null,`.ROBLOSECURITY=${this.roblosecurity}`).then(res => res.json())
+        return fetch("https://www.roblox.com/user/favorites/places",`.ROBLOSECURITY=${this.roblosecurity}`).then(res => res.json())
     }
     getAccountDetails() {
-        return fetchJSON("https://www.roblox.com/my/account/json",null,`.ROBLOSECURITY=${this.roblosecurity}`).then(res => res.json())
+        return fetch("https://www.roblox.com/my/account/json",`.ROBLOSECURITY=${this.roblosecurity}`).then(res => res.json())
     }
     getAccountSettings() {
-        return fetchJSON("https://www.roblox.com/my/settings/json",null,`.ROBLOSECURITY=${this.roblosecurity}`).then(res => res.json())
+        return fetch("https://www.roblox.com/my/settings/json",`.ROBLOSECURITY=${this.roblosecurity}`).then(res => res.json())
     }
     getFriendsOnline() {
-        return fetchJSON("https://api.roblox.com/my/friendsonline", null, `.ROBLOSECURITY=${this.roblosecurity}`).then(res => res.json())
+        return fetch("https://api.roblox.com/my/friendsonline",`.ROBLOSECURITY=${this.roblosecurity}`).then(res => res.json())
     }
     getAccountInfo() {
-        return fetchJSON("https://api.roblox.com/users/account-info", null, `.ROBLOSECURITY=${this.roblosecurity}`).then(res => res.json())
+        return fetch("https://api.roblox.com/users/account-info",`.ROBLOSECURITY=${this.roblosecurity}`).then(res => res.json())
     }
     getMobileApiInfo() {
-        return fetchJSON("https://www.roblox.com/mobileapi/userinfo",null,`.ROBLOSECURITY=${this.roblosecurity}`).then(res => res.json())
+        return fetch("https://www.roblox.com/mobileapi/userinfo",`.ROBLOSECURITY=${this.roblosecurity}`).then(res => res.json())
     }
 }
 
@@ -107,21 +91,24 @@ class Place {
         if (this.checkStatus() == false) return console.error(`Place doesn't exist.`)
     }
     checkStatus() {
-        return fetchJSON(`https://www.roblox.com/places/api-get-details?assetId=${this.place}`)
+        return fetch(`https://www.roblox.com/places/api-get-details?assetId=${this.place}`)
             .then(res => {
-                if (res.json().errors.code) {
+                if (res.json().errors) {
                     return false
                 } else return true
             })
     }
+    getGameInstances() {
+        return fetch(`https://games.roblox.com/v1/games/${this.place}/servers/Public?excludeFullGames=false&limit=100`).then(res => res.json());
+    }
     getDetails() {
-        return fetchJSON(`https://www.roblox.com/places/api-get-details?assetId=${this.place}`).then(res => res.json())
+        return fetch(`https://www.roblox.com/places/api-get-details?assetId=${this.place}`).then(res => res.json())
     }
     getSettings() {
-        return fetchJSON(`https://www.roblox.com/places/${this.place}/settings`).then(res => res.json())
+        return fetch(`https://www.roblox.com/places/${this.place}/settings`).then(res => res.json())
     }
     getUniverse() {
-        return fetchJSON(`https://api.roblox.com/universes/get-universe-containing-place?placeid=${this.place}`).then(res => res.json())
+        return fetch(`https://api.roblox.com/universes/get-universe-containing-place?placeid=${this.place}`).then(res => res.json())
     }
 }
 
@@ -133,21 +120,24 @@ class Universe {
     }
     checkStatus() {
         // https://api.roblox.com/universes/get-info?universeId=13058
-        return fetchJSON(`https://api.roblox.com/universes/get-info?universeId=${this.universe}`)
+        return fetch(`https://api.roblox.com/universes/get-info?universeId=${this.universe}`)
             .then(res => {
-                if (res.json().errors.code) {
+                if (res.json().errors) {
                     return false
                 } else return true
             })
     }
+    getPrice() {
+        return fetch(`https://games.roblox.com/v1/games/games-product-info?universeIds=${this.universe}`).then(res => res.json());
+    }
     getInfo() {
-        return fetchJSON(`https://api.roblox.com/universes/get-info?universeId=${this.universe}`).then(res => res.json())
+        return fetch(`https://api.roblox.com/universes/get-info?universeId=${this.universe}`).then(res => res.json())
     }
     getPlaces() {
-        fetchJSON(`https://api.roblox.com/universes/get-universe-places?universeId=${this.universe}&page=1`)
+        return fetch(`https://api.roblox.com/universes/get-universe-places?universeId=${this.universe}&page=1`)
             .then(res => {
                 let req = res.json();
-                if (r.FinalPage == false) {
+                if (req.FinalPage == false) {
                     let pageNum = 2;
                     let endLoop = false;
                     while (endLoop == false) {
@@ -164,7 +154,6 @@ class Universe {
 }
 module.exports = {
     Player,
-    User,
     Place,
     Universe
 }
